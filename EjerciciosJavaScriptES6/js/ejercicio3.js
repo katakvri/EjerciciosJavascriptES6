7a528ef40c7ca7797c6f0c5d8f832273b39f8263
martinveri <68635436+katakvri@users.noreply.github.com>
2021-05-26 19:19:26 +0200

    Add files via upload

---
+++ EjerciciosJavaScriptES6/js/ejercicio3.js
@@ -0,0 +1,4 @@
+function counterChar() {
+    const replacedSpaces = string.replace(/\s/g, ""); 
+    const splittedArray = replacedSpaces.split("");
+}
---
+++ EjerciciosJavaScriptES6/js/ejercicio6.js
@@ -0,0 +1,7 @@
+var name = prompt("Escribe tu nombre para convertirlo a CodigoNombre")
+
+function nameCode() {
+    name.splice(2,this.length)
+};
+
+console.log(nameCode())
