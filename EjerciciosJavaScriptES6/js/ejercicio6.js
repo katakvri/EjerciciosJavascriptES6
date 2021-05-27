75b917d75ab4b4f51289a904c1b4f4e19cdad8ff
mverikas <68635436+katakvri@users.noreply.github.com>
2021-05-27 19:01:52 +0200

    Update ejercicio6.js

--- EjerciciosJavaScriptES6/js/ejercicio6.js
+++ EjerciciosJavaScriptES6/js/ejercicio6.js
@@ -1,8 +1,15 @@
-var name = prompt("Escribe tu nombre para convertirlo a CodigoNombre")
-var count = {};
+var robotName = prompt("Escribe tu nombre para convertirlo a CodigoNombre");
+const alphabet = ["0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
 
-function nameCode() {
-    name.splice(2,this.length)
+function nameCode(string) {
+    const stringNoSpaces = string.replace(/\s/g, "");
+    const stringUpper = stringNoSpaces.toUpperCase();
+    const name = stringUpper.slice(0, 2);
+    const nameSplitted = name.split("");
+    nameSplitted.push(alphabet.indexOf(nameSplitted[0]));
+    nameSplitted.push(alphabet.indexOf(nameSplitted[1]));
+    return nameSplitted.join("");
+
 };
 
-console.log(nameCode())
+console.log(nameCode(robotName));
